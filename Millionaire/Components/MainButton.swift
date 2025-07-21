e6ba67dd5d72730964c92998397cd70dcab65011
SnakCat <snakkdss@gmail.com>
2025-07-21 19:35:15 +0300

    создание главной кнопки

---
+++ Millionaire/Components/MainButton.swift
@@ -0,0 +1,18 @@
+//
+//  MainButton.swift
+//  Millionaire
+//
+//  Created by DimaTru on 21.07.2025.
+//
+
+import SwiftUI
+
+struct MainButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainButton()
+}
