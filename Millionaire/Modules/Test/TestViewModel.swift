41ef6e280f43a81dcae828aa3669ad117dd6da36
nikita <nikmosyl@gmail.com>
2025-07-21 19:23:10 +0300

    изменил иерархию проекта

---
+++ Millionaire/Modules/Test/TestViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TestViewModel.swift
+//  Millionaire
+//
+//  Created by nikita on 21.07.2025.
+//
+
+import Foundation
---
+++ Millionaire/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  Millionaire
+//
+//  Created by nikita on 21.07.2025.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
