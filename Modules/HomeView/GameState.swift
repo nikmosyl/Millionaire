257a73111eb1df4c4d1a6906d1a08791c1f79be0
Aleksandr-Zhazhoyan <zhazhoyan85@gmail.com>
2025-07-22 21:04:17 +0400

    Home View is done

---
+++ Modules/HomeView/GameState.swift
@@ -0,0 +1,13 @@
+//
+//  GameState.swift
+//  Millionaire
+//
+//  Created by Aleksandr Zhazhoyan on 22.07.2025.
+//
+
+import Foundation
+
+struct GameState {
+    var bestScore: Int
+    var continueGame: Bool
+}
