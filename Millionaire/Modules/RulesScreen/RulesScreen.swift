182b98edec91848169ffe00d5a0f265151884ca0
RookCYP <general.ludd.vs@gmail.com>
2025-07-22 21:06:54 +0300

    Обновил экран Правил

--- Millionaire/Modules/RulesScreen/RulesScreen.swift
+++ Millionaire/Modules/RulesScreen/RulesScreen.swift
@@ -44,38 +44,55 @@ struct ContentView: View {
         NavigationView {
             ZStack {
                 Color("RulesBackground").ignoresSafeArea()
-                VStack {
-                    NavigationLink(destination: ContentView(), label: {
-                        Image(systemName: "chevron.left")
-                            .font(.title)
-                           
-                
-                       
+                VStack(alignment:.leading) {
+                    
+                    HStack {
+                        NavigationLink(destination: ContentView(), label: {
+                            Image(systemName: "chevron.left")
+                                .font(.title)
+                            
                             Text("Назад")
                                 .foregroundColor(.blue)
                                 .font(.headline)
-                    })
-                    .padding(.trailing, 300.0)
-                   
+                                .multilineTextAlignment(.leading)
+                            
+                            VStack {
+                                Text("Правила")
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                                    .padding(.horizontal, 60)
+                            }
+                        })
+                    }
+                  
                     
+     
                     VStack(alignment:.center, spacing: 20) {
-                        Section {
-                            Text("Правила")
-                                .font(.title)
-                                .foregroundColor(.white)
-                        }
+                        
                         ScrollView{
                             Text("""
-                                Варианты ответов: к каждому вопросу даётся 4 варианта, правильный из них — лишь один.
+                                Варианты ответов:
+                                
+                                к каждому вопросу даётся 4 варианта, правильный из них — лишь один.
+                                
                                 Стоимость вопросов: первые 5 вопросов — лёгкие, с 6 по 10 — серьёзные, но взяты из общих областей знаний, наиболее сложные — с 11 по 15.
+                                
                                 Несгораемая сумма: её нельзя проиграть, даже если был дан неправильный ответ.
+                                
                                 Подсказки: на всю игру участнику даётся 3 подсказки:
                                 «50 на 50» — компьютер скроет два ошибочных ответа, оставив лишь правильный вариант и один неправильный.
+                                
                                 «Звонок другу» — возможность позвонить любому знакомому и попросить подсказать ответ.
+                                
                                 «Помощь зала» — зрители, пришедшие посмотреть игру, подсказывают свои варианты ответов, которые при желании можно принять.
+                                
                                 Остановка игры: в любой момент можно остановить игру и забрать заработанные на данный момент деньги.
                                 """).font(.title)
+                                .italic(true)
                                 .foregroundColor(.white)
+                                .multilineTextAlignment(.leading)
+                                .padding()
+                                
                         }
                     }
                     }
