3ba9219c48c6cdde669c0ffa4969b273b071dceb
nikita <nikmosyl@gmail.com>
2025-07-23 18:35:48 +0300

    change prj scheme

--- Millionaire/Modules/RulesScreen/RulesScreen.swift
+++ Millionaire/Modules/RulesScreen/RulesScreen.swift
@@ -6,23 +6,21 @@
 //
 
 import SwiftUI
-    
 
-    struct RulesScreen: View {
-   
+struct RulesScreen: View {
     var body: some View {
         NavigationView {
             ZStack {
                 Color("RulesBackground").ignoresSafeArea()
-                VStack(alignment:.leading) {
+                
+                VStack(alignment: .leading) {
                     
                     HStack {
                         NavigationLink(destination: HomeView().navigationBarBackButtonHidden(true), label: {
-
+                            
                             Image(systemName: "chevron.left")
                                 .padding(.leading)
                                 .font(.subheadline)
-                                
                             
                             Text("Back")
                                 .foregroundColor(.blue)
@@ -37,18 +35,15 @@ import SwiftUI
                             }
                         })
                     }
-                  
                     
-     
                     VStack(alignment:.center, spacing: 20) {
-                        
                         ScrollView(showsIndicators: false){
                             Section(
                                 header: Text("Qualifying round")
                                     .font(.title3)
                                     .bold()
-                                    , content: {
-                                Text("""
+                                , content: {
+                                    Text("""
                                     
                                     Selection format: 
                                     Participants answer a question with four possible answers, placing them in the correct order. 
@@ -57,14 +52,16 @@ import SwiftUI
                                     Cancel button: players can correct their answer.
                                     
                                     """)
-                            })
+                                })
+                            
                             Spacer()
+                            
                             Section(
                                 header: Text("Main Game")
-                                .font(.title3)
-                                .bold()
-                                    , content: {
-                                Text("""
+                                    .font(.title3)
+                                    .bold()
+                                , content: {
+                                    Text("""
                                     
                                     Main prize: 3 million rubles
                                     Number of questions: 15
@@ -72,7 +69,7 @@ import SwiftUI
                                     Difficulty: the first 5 questions are easy, 6-10 are medium difficulty, and 11-15 are the most difficult
                                     
                                     """)
-                            })
+                                })
                             
                             Spacer()
                             Section(
@@ -80,37 +77,39 @@ import SwiftUI
                                     .font(.title3)
                                     .bold()
                                 , content: {
-                                Text("""
+                                    Text("""
                                     
                                     50-50: the computer removes two incorrect answers
                                     Calling a friend: you can call and consult for 30 seconds
                                     Help from the audience: viewers vote for the correct answer
                                     
                                     """)
-                            })
+                                })
                             
                             Spacer()
+                            
                             Section(
                                 header: Text("Winning System")
                                     .font(.title3)
                                     .bold()
                                 , content: {
-                                Text("""
+                                    Text("""
                                     
                                     Non-burnable amount: a certain amount that cannot be lost
                                     Stop game: the player can stop the game at any time and collect the money they have earned
                                     Using hints: each hint can only be used once
                                     
                                     """)
-                            })
+                                })
                             
                             Spacer()
+                            
                             Section(
                                 header: Text("Additional rules")
                                     .font(.title3)
                                     .bold()
                                 , content: {
-                                Text("""
+                                    Text("""
                                     
                                     Participation format: Starting from July 2025, the most knowledgeable and persistent players will enter the game.
                                     Final siren: a signal that prompts the player to provide a final answer
@@ -118,32 +117,20 @@ import SwiftUI
                                     Right to error: not available in the current version of the game
                                     Question replacement: also not available in the current version
                                     """)
-                            })
-                            
-                            
-                            
+                                })
                         }.foregroundColor(.white)
                             .font(.system(size: 17, weight: .light, design: .serif))
-                            
+                        
                             .padding(20)
                     }
                     
-                    }
-                 
-                    Spacer()
                 }
-        }.navigationBarBackButtonHidden(true)
-        
-            
                 
-        }
+                Spacer()
+            }
+        }.navigationBarBackButtonHidden(true)
     }
-
-
-        
-            
-     
-
+}
 
 #Preview {
     RulesScreen()
