49c07e234de5c99d28db93a225fa4e9db9e32816
nikita <nikmosyl@gmail.com>
2025-07-25 21:01:10 +0300

    ренейминг

--- Millionaire/Modules/HomeView/HomeView.swift
+++ Millionaire/Modules/HomeView/HomeView.swift
@@ -49,7 +49,7 @@ struct HomeView: View {
                 
                 if viewModel.gameState.continueGame {
                     MainButton(style: .yellow) {
-                        viewModel.continGame()
+                        viewModel.continueGame()
                         navigateToQuestion = false
                     } content: {
                         Text("Continue Game")
